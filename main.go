246071cff6a9c447e3d1ed84ee4da5609e17322b
misua <charlespogzz@googlemail.com>
2022-09-29 22:39:38 +0800

    its not =+, its += dumbdumb

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ type Position struct {
 	Y int
 }
 
+func (p *Position) Move(dx, dy int) {
+	p.X += dx
+	p.Y += dy
+
+}
+
 func main() {
-	p := Position{X: 10, Y: 20}
+	p := &Position{X: 10, Y: 20}
 	p.Move(5, -5)
 }
